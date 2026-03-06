660ce09d2cc874c757cf5716816e6114e344ae3f
patilsagar22301-maker <patilsagar22301@gmail.com>
2026-03-06 00:06:04 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,20 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
+    host: true,
+    port: 8080
   },
   build: {
-    outDir: "dist/spa",
+    outDir: "dist"
   },
-  plugins: [react(), 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
-}));
+      "@shared": path.resolve(__dirname, "./shared")
+    }
+  }
+});
